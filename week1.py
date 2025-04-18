d6f1e6b41f2d91708d51f33f0309205deefdf787
MosesMolepo001 <160005561+MosesMolepo001@users.noreply.github.com>
2025-04-18 07:53:31 +0200

    Update week1.py

--- week1.py
+++ week1.py
@@ -1,1 +1,12 @@
+from pet import Pet
+
+my_pet = Pet("Spike")
+
+my_pet.get_status()
+my_pet.eat()
+my_pet.sleep()
+my_pet.play()
+my_pet.train("roll over")
+my_pet.show_tricks()
+my_pet.get_status()
 
